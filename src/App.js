c0e623e8f0413e5d7ff817c384561d4ac11a41a4
tubiky <61419018+tubiky@users.noreply.github.com>
2020-10-27 07:01:05 +0900

    Finished 2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+/* "./" means the same directory. */
+
+function Food(props){
+  return <h1> I like {props.fav}.</h1>
+}
+
 function App() {
   return (
-    <div> Hello!!!!</div>);
+    <div> 
+      <h1>Hello!!!!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="라면"/>
+      <Food fav="samgiopsal"/>
+    </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+ /* important command
+   1. npx create-react-app NAME_OF_THE_APPLICATOIN 
+   2. npm start : run server
+   3. npm run build : to create a production build
+   4. git init
+   5. git remot add origin "URL from github"
+   6. git add .
+   7. git commit -m "Message"
+   8. git push origin master
+   */
+
+ /* App in line 9 is a component. It is related to function App() in App.js*/
+ /* This combination of Java Script and HTML is called JSX, which is only specific
+ in React. */
+
+ /* Whenever you make a component you need to "import React from 'react';" */
+
+ /* React application can render only ONE COMPONENT at a time. */
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />  
   </React.StrictMode>,
   document.getElementById('root')
 );
